a193dda2836b34e94c5c7cfc3a1573e75050d3d7
Gil Alpert <alpertgil@gmail.com>
2024-10-18 02:42:14 +0300

    Okay it was my bad I copy pasted and forgot to change

--- client/EncryptedFileTransferingSystemImprovedClient/EncryptedFileTransferingSystemImprovedClient/Protocol/Responses/GotFileWithCrcResponse.cpp
+++ client/EncryptedFileTransferingSystemImprovedClient/EncryptedFileTransferingSystemImprovedClient/Protocol/Responses/GotFileWithCrcResponse.cpp
@@ -6,7 +6,7 @@ void protocol::GotFileWithCrcResponse::deserialize(const buffer::Buffer& data)
 	SignupSuccessResponse::deserialize(data);
 	m_content_size = deserialize_uint32(data.begin() + SignupSuccessResponse::size());
 	m_filename = std::string(data.begin() + SignupSuccessResponse::size() + sizeof(m_content_size), data.begin() + SignupSuccessResponse::size() + sizeof(m_content_size) + FILENAME_SIZE);
-	m_content_size = deserialize_uint32(data.begin() + SignupSuccessResponse::size() + sizeof(m_content_size) + FILENAME_SIZE);
+	m_checksum = deserialize_uint32(data.begin() + SignupSuccessResponse::size() + sizeof(m_content_size) + FILENAME_SIZE);
 }
 
 uint32_t protocol::GotFileWithCrcResponse::get_content_size() const
