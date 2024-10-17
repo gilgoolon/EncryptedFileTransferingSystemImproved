85150b1cebe7fdc952578fce8be223f1de61c47a
Gil Alpert <alpertgil@gmail.com>
2024-10-18 02:29:47 +0300

    Once again... I am not kidding... Fixed CryptoPP stupidity

--- client/EncryptedFileTransferingSystemImprovedClient/EncryptedFileTransferingSystemImprovedClient/Crypto/Crc.cpp
+++ client/EncryptedFileTransferingSystemImprovedClient/EncryptedFileTransferingSystemImprovedClient/Crypto/Crc.cpp
@@ -1,18 +1,16 @@
 #include <cryptopp/crc.h>
+#include <cryptopp/crc.h>
+#include <cryptopp/hex.h>
+#include <cryptopp/filters.h>
 
 #include "Crc.hpp"
+#include "../Common/Logging/Logger.hpp"
 
 uint32_t crypto::crc::calculate(const buffer::Buffer& data)
 {
     CryptoPP::CRC32 crc;
+    uint32_t checksum = 0;
     crc.Update(data.data(), data.size());
-    uint8_t digest[CryptoPP::CRC32::DIGESTSIZE] = { 0 };
-    crc.Final(digest);
-
-    uint32_t crc32_value = 0;
-    for (int i = 0; i < CryptoPP::CRC32::DIGESTSIZE; ++i) {
-        crc32_value = (crc32_value << 8) | digest[i];
-    }
-
-    return crc32_value;
+    crc.TruncatedFinal(reinterpret_cast<buffer::Byte*>(&checksum), sizeof(checksum));
+    return checksum;
 }
